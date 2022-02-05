a8ba6261f58ae98b59dca0bcf731132747fb8095
rewine <lhongxu@outlook.com>
2022-02-06 00:37:25 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A very very very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nvfetcher= {
+      url = "github:berberman/nvfetcher";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nvfetcher, flake-utils }: {
+     let
+      genPkg = f: name: {
+        inherit name;
+        value = f name;
+      };
+      pkgDir = ./packages;
+      names = with builtins; attrNames (readDir pkgDir);
+      withContents = f: with builtins; listToAttrs (map (genPkg f) names);
+    in {
+      overlay = final: prev:
+        withContents (name:
+          final.callPackage (pkgDir + "/${name}") {
+            pythonPackages = final.python3.pkgs;
+          });
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in { packages = withContents (name: pkgs.${name}); }
+  };
+}
