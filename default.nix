b2bded9ccf60a043d6354dc22f60fe3d7a82e4f2
rewine <1758075541@qq.com>
2022-06-19 03:34:25 +0800

    cmdmarkdown: init at 2017-01-14

--- default.nix
+++ default.nix
@@ -4,4 +4,5 @@
   typora-legacy = pkgs.callPackage ./pkgs/typora-legacy { };
   landrop = pkgs.callPackage ./pkgs/landrop { };
   graphbuilder = pkgs.callPackage ./pkgs/graphbuilder { };
+  cmdmarkdown = pkgs.callPackage ./pkgs/cmd-markdown { };
 }
