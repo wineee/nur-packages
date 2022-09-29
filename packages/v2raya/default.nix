e3c2e245850bd00b4be56d544119ae94c618c96a
rewine <1758075541@qq.com>
2022-09-29 23:48:58 +0800

    Revert "v2raya: 1.5.9.1698.1 -> 1.5.10(fake)"
    This reverts commit 36fd04ff9f5900b1f4fb952fed0fe0a0b1deaf16.

--- packages/v2raya/default.nix
+++ packages/v2raya/default.nix
@@ -11,19 +11,19 @@
 }:
 let
   pname = "v2raya";
-  version = "1.5.10"; # unstable
+  version = "1.5.9.1698.1";
   src = fetchFromGitHub {
     owner = "v2rayA";
     repo = "v2rayA";
-    rev = "24e42a9fc4b9437c451656c117ea6e7f888e59d4";
-    sha256 = "sha256-r27FpYHnGSKHFhlQh8pFYoGqXENtYvGU4cNJ9zs+JJs=";
+    rev = "v${version}";
+    sha256 = "sha256-h0ZYp/QY+UhQmhCiRkUAGy9zlkmDY7h+QxNzYvweJz0=";
   };
   web = mkYarnPackage {
     inherit pname version;
     src = "${src}/gui";
     offlineCache = fetchYarnDeps {
       yarnLock = src + "/gui/yarn.lock";
-      sha256 = "";
+      sha256 = "sha256-2n9qD9AsMPplyhguVFULq7TQYpOpsrw6XXjptbOaYF8=";
     };
     packageJSON = ./package.json;
 
@@ -41,7 +41,7 @@ in
 buildGoModule {
   inherit pname version;
   src = "${src}/service";
-  vendorSha256 = "";
+  vendorSha256 = "sha256-RqpXfZH0OvoG0vU17oAHn1dGLQunlUJEW89xuCSGEoE=";
   subPackages = [ "." ];
   nativeBuildInputs = [ makeWrapper ];
   preBuild = ''
