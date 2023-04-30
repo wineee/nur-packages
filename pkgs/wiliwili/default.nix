70293afe65bfe7a4447dde9318651a617eda2d9a
rewine <lhongxu@outlook.com>
2023-05-01 00:24:45 +0800

    wiliwili: 0.6.0 -> 1.0.0

--- pkgs/wiliwili/default.nix
+++ pkgs/wiliwili/default.nix
@@ -1,37 +1,47 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, libsForQt515
+, libsForQt5
 , pkg-config
 , cmake
 , mpv
+, dbus
+, libwebp
 , python3
 , xorg
 , SDL2
 , mbedtls
 , openssl
+, useDsl ? true
 }:
 let
-  wrapQtAppsHook = libsForQt515.qt5.wrapQtAppsHook;
+  wrapQtAppsHook = libsForQt5.qt5.wrapQtAppsHook;
   inherit ((builtins.getFlake "github:NixOS/nixpkgs/23485f23ff8536592b5178a5d244f84da770bc87").legacyPackages.${stdenv.system}) curl;
 in
 stdenv.mkDerivation rec {
   pname = "wiliwili";
-  version = "0.6.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "xfangfang";
     repo = "wiliwili";
-    rev = "07548e2cd65eb51271bd7d8ad11f60f18767d425";
+    rev = "v${version}";
     fetchSubmodules = true;
-    hash = "sha256-jWnvI2kUdIzxyx2Q7712owvpgLwjHcFfg3G+JA6GqWU=";
+    hash = "sha256-PO4qzdf71gNEpVI7809fhF0shky8CjXt1sttOWEDTDQ=";
   };
 
-  nativeBuildInputs = [ cmake wrapQtAppsHook python3 ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    wrapQtAppsHook
+    python3
+  ];
 
   buildInputs = [ 
     mpv
+    dbus
     curl
+    libwebp
     xorg.libXrandr
     xorg.libXinerama
     xorg.libXcursor
@@ -45,13 +55,15 @@ stdenv.mkDerivation rec {
     "-DUSE_SYSTEM_CURL=ON"
     "-DWIN32_TERMINAL=OFF"
     "-DINSTALL=ON"
-    "-DUSE_SDL2=ON"
+    "-DUSE_SDL2=${if useDsl then "ON" else "OFF"}"
   ];
 
   meta = with lib; {
     description = "Yet another Bilibili client";
     homepage = "https://github.com/xfangfang/wiliwili";
     license = licenses.mit;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ rewine ];
   };
 }
 
