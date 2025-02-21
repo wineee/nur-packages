b738616260400e27834866797a751f699bee6f6f
rewine <luhongxu@deepin.org>
2025-02-22 00:35:24 +0800

    peony: drop

--- default.nix
+++ default.nix
@@ -14,10 +14,6 @@ with pkgs; rec {
   wayland-debug = pkgs.callPackage ./pkgs/wayland-debug {
     wayland = pkgs.callPackage ./pkgs/libwayland-for-debug { };
   };
-  #ukui-interface = pkgs.libsForQt5.callPackage ./pkgs/ukui-interface { };
-  #libkysdk-base = pkgs.libsForQt5.callPackage ./pkgs/libkysdk-base { };
-  #libkysdk-applications = pkgs.libsForQt5.callPackage ./pkgs/libkysdk-applications { inherit libkysdk-base; };
-  #peony = pkgs.libsForQt5.callPackage ./pkgs/peony { inherit ukui-interface; };
   #mogan = pkgs.libsForQt5.callPackage ./pkgs/mogan {  };
   nowide = pkgs.callPackage ./pkgs/nowide {  };
   kylin-virtual-keyboard = libsForQt5.callPackage ./pkgs/kylin-virtual-keyboard { };
