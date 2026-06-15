8623ae0598a2fe893724a9e6d35ee1e43758a48a
rewine <luhongxu@deepin.org>
2026-06-15 13:05:37 +0800

    aish: drop

--- default.nix
+++ default.nix
@@ -30,5 +30,4 @@ with pkgs; rec {
   deepin-translation-utils = pkgs.callPackage ./pkgs/deepin-translation-utils { };
   wlanalyze = pkgs.callPackage ./pkgs/wlanalyze { };
   lazycat-terminal = pkgs.callPackage ./pkgs/lazycat-terminal { };
-  #aish = pkgs.callPackage ./pkgs/aish { };
 }
